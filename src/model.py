99dcda36246ffe324ec5dd53366c9b86ddead86b
ZYang16 <34953056+zyang-16@users.noreply.github.com>
2022-11-02 10:25:09 +0800

    Add files via upload
    fix gradient issue

--- src/model.py
+++ src/model.py
@@ -1,5 +1,3 @@
-from tensorflow.python.keras.backend import dtype
-from tensorflow.python.ops.gen_array_ops import reshape
 from layers import *
 from parser import parse_args
 import numpy as np
@@ -82,42 +80,39 @@ class STAM4Rec(Model):
         self.n_heads = args.n_heads
         self.hidden_dim = args.hidden_dim
         self.decay = args.decay
-        self.dim = args.dim
+        self.dim = args.dim 
         self.n_fold = 10
         self.batch_size = args.batch_size
         self.n_users = data_config['n_users']
         self.n_items = data_config['n_items']  
         self.norm_adj = data_config['norm_adj']
+        self.mask = data_config['temporal_index']
         self.temporal_table = tf.Variable(data_config['temporal_table'], trainable=False, name="temporal_table") # [M+N, S]
         self.placeholders = placeholders 
-        print("self.num_layers", self.num_layers)
         self.indices = np.squeeze(np.stack((np.repeat(np.arange(0, self.n_users+self.n_items), self.input_length).reshape(-1,1), data_config['temporal_table'].reshape(-1,1)), 1))
-        print("self.indices", self.indices)
         print("self.norm_adj", self.norm_adj)
 
         self.users = self.placeholders['users'] # [1, batch_size] idx: 0 ~ (M-1)
         self.pos_items = self.placeholders['pos_items'] # [1, batch_size] idx: M ~ (M+N-1)
         self.neg_items = self.placeholders['neg_items'] # [1, batch_size] idx: M ~ (M+N-1)
-        self.A_new = tf.sparse_placeholder(tf.float32, shape=(self.n_users+self.n_items, self.n_users+self.n_items), name='alpha_sp')
 
         # user embedding matrix and item embedding matrix
-        initializer = tf.contrib.layers.xavier_initializer() 
-        # initializer = tf.random_normal_initializer(stddev=0.01)
+        initializer = tf.random_normal_initializer(stddev=0.01)
         self.user_embeddings = tf.Variable(initializer([self.n_users, self.input_dim]), name='user_embeddings') # [M, d]
         self.item_embeddings = tf.Variable(initializer([self.n_items, self.input_dim]), name='item_embeddings') # [N, d]
-
-        self.stam_layer = STAM(input_dim=self.input_dim, n_heads=self.n_heads,
-                          input_length=self.input_length, hidden_dim=self.hidden_dim)
         
-        self.init_embeddings = tf.concat([self.user_embeddings, self.item_embeddings], axis=0) # [M+N, d] 
+        self.stam_layer = STAM(input_dim=self.input_dim, n_heads=self.n_heads,
+                              input_length=self.input_length, hidden_dim=self.hidden_dim)
 
-        print("self.init_embeddings", self.init_embeddings)
+        self.W_Q = self.stam_layer.vars['Q_embedding']
+
+        self.init_embeddings = tf.concat([self.user_embeddings, self.item_embeddings], axis=0) # [M+N, d] 
 
         self.user_embeddings_pre = tf.nn.embedding_lookup(self.user_embeddings, self.users)
         self.pos_item_embeddings_pre = tf.nn.embedding_lookup(self.init_embeddings, self.pos_items) # M~(M+N-1)
         self.neg_item_embeddings_pre = tf.nn.embedding_lookup(self.init_embeddings, self.neg_items) # M~(M+N-1)
 
-        self.stam_weights = self.get_stam_weights()
+        self.A_new = self.get_stam_weights()
 
         self._build()
     
@@ -144,20 +139,18 @@ class STAM4Rec(Model):
         indices = np.mat([coo.row, coo.col]).transpose() 
         return tf.SparseTensor(indices, coo.data, coo.shape) 
 
-    # concat + L2 + mean-pooling for neighbor aggregation
 
+    # concat + L2 + mean-pooling for neighbor aggregation
     def get_stam_weights(self):
         # obtain spatiotemporal attention weights [M+N, M+N]
         length = self.n_users+self.n_items
         stam_batch_size = self.batch_size // 8
         batch_num = int(length // stam_batch_size)
-        print("batch_num: ", batch_num)
         for i in range(batch_num):
             start = i * stam_batch_size
             end = (i+1) * stam_batch_size
             input_idx_batch = tf.range(start, end)
             stam_inputs = self.get_temporal_neighbors(input_idx_batch) # [batch_size, S, d] 
-            print("stam_inputs:", stam_inputs)
             # STAM forward  
             stam_outs_batch = self.stam_layer(stam_inputs) # spatiotemporal neighbor embeddings [batch_size, S, d]
             W_1 = tf.expand_dims(tf.nn.embedding_lookup(self.init_embeddings, input_idx_batch), 1) #[batch_size, 1, d]
@@ -172,15 +165,21 @@ class STAM4Rec(Model):
         W_1 = tf.expand_dims(tf.nn.embedding_lookup(self.init_embeddings, input_idx_last), 1)
         stam_weights_batch = tf.nn.softmax(tf.squeeze(tf.matmul(stam_outs_batch, tf.transpose(W_1, [0, 2, 1]))), 1)
         stam_weights = tf.concat([stam_weights, stam_weights_batch], 0)
-        print("stam_weights: ", stam_weights)
-        self.stam_weights = tf.SparseTensor(indices=self.indices,
-                                            values=tf.squeeze(tf.reshape(stam_weights, [1,-1])),
+
+        # stam_weights -> adj 
+        adj_mask = tf.multiply(stam_weights, self.mask) 
+        adj_sum = tf.div_no_nan(adj_mask, tf.reduce_sum(adj_mask, 1, keep_dims=True))
+        adj = tf.boolean_mask(adj_sum, self.mask)
+        adj_indices = tf.boolean_mask(self.indices, self.mask.reshape(1,-1)[0])
+        
+        self.stam_weights = tf.SparseTensor(indices=adj_indices,
+                                            values=adj,
                                             dense_shape=[self.n_users+self.n_items, self.n_users+self.n_items])
         return self.stam_weights
 
     def build_net(self): 
         # stacking L layers     
-        ego_embeddings = self.init_embeddings
+        ego_embeddings = self.init_embeddings 
         all_embeddings = [ego_embeddings] 
         for k in range(0, self.num_layers): 
             side_embeddings = tf.sparse_tensor_dense_matmul(self.A_new, ego_embeddings)
@@ -210,6 +209,7 @@ class STAM4Rec(Model):
 
     def init_optimizer(self):
         trainable_params = tf.trainable_variables()
+        print("trainable_params", trainable_params)
         gradients = tf.gradients(self.bpr_loss, trainable_params)
         clipped_grads_and_vars, _ = tf.clip_by_global_norm(gradients, args.max_gradient_norm)
         self.optimizer = tf.train.AdamOptimizer(learning_rate=args.lr)
